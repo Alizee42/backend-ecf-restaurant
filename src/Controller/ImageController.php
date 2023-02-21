f34c217f242b2c0b6aafc84b5823d1797e9615a3
Alizee42 <alizee.gueye@gmail.com>
2023-02-21 21:07:23 +0100

    Implementation controllers

--- src/Controller/ImageController.php
+++ src/Controller/ImageController.php
@@ -2,17 +2,106 @@
 
 namespace App\Controller;
 
+use App\Repository\ImageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ImageController extends AbstractController
 {
-    #[Route('/image', name: 'app_image')]
-    public function index(): Response
+    private $imageRepository;
+
+    public function __construct(ImageRepository $imageRepository)
     {
-        return $this->render('image/index.html.twig', [
-            'controller_name' => 'ImageController',
-        ]);
+        $this->imageRepository = $imageRepository;
+    }
+
+    /**
+     * @Route("/images", name="add_image", methods={"POST"})
+     */
+    public function add(Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $titre = $data['titre'];
+        $path = $data['path'];
+        
+
+        if (empty($titre)) {
+            throw new NotFoundHttpException('Bad request');
+        }
+
+        $this->imageRepository->save($titre, $path);
+
+        return new JsonResponse(['status' => 'Image created!'], Response::HTTP_CREATED);
+    }
+
+    /**
+     * @Route("/images/{id}", name="get_one_image", methods={"GET"})
+     */
+    public function get($id): JsonResponse
+    {
+        $image = $this->imageRepository->findOneBy(['id' => $id]);
+
+        $data = [
+            'id' => $image->getId(),
+            'titre' => $image->getTitre(),
+            'path'=>$image->getPath(),
+            
+        ];
+
+        return new JsonResponse($data, Response::HTTP_OK);
+    }
+
+    /**
+     * @Route("/images", name="get_all_images", methods={"GET"})
+     */
+    public function getAll(): JsonResponse
+    {
+        $images = $this->imageRepository->findAll();
+        $data = [];
+
+        foreach ($images as $image) {
+            $data[] = [
+            'id' => $image->getId(),
+            'titre' => $image->getTitre(),
+            'path'=>$image->getPath(),
+            
+            ];
+        }
+
+        return new JsonResponse($data, Response::HTTP_OK);
+    }
+
+    /**
+     * @Route("/images/{id}", name="update_image", methods={"PUT"})
+     */
+    public function update($id, Request $request): JsonResponse
+    {
+        $image = $this->imageRepository->findOneBy(['id' => $id]);
+        $data = json_decode($request->getContent(), true);
+
+        empty($data['titre']) ? true : $image->setTitre($data['titre']);
+        empty($data['path']) ? true : $image->setPath($data['path']);
+        
+
+        $updatedImage = $this->imageRepository->update($image);
+
+        return new JsonResponse($updatedImage, Response::HTTP_OK);
+    }
+
+    /**
+     * @Route("/images/{id}", name="delete_image", methods={"DELETE"})
+     */
+    public function delete($id): JsonResponse
+    {
+        $image = $this->imageRepository->findOneBy(['id' => $id]);
+
+        $this->imageRepository->remove($image);
+
+        return new JsonResponse(['status' => 'Image deleted'], Response::HTTP_NO_CONTENT);
     }
 }
--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -2,17 +2,115 @@
 
 namespace App\Controller;
 
+use App\Repository\ReservationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ReservationController extends AbstractController
 {
-    #[Route('/reservation', name: 'app_reservation')]
-    public function index(): Response
+    private $reservationRepository;
+
+    public function __construct(ReservationRepository $reservationRepository)
     {
-        return $this->render('reservation/index.html.twig', [
-            'controller_name' => 'ReservationController',
-        ]);
+        $this->reservationRepository = $reservationRepository;
+    }
+
+    /**
+     * @Route("/reservations", name="add_reservation", methods={"POST"})
+     */
+    public function add(Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $numero = $data['numero'];
+        $date = $data['date'];
+        $heurePrevu = $data['heurePrevu'];
+        $nombreConvive = $data['nombreConvive'];
+        $allergie = $data['allergie'];
+
+
+        if (empty($numero)) {
+            throw new NotFoundHttpException('Bad request');
+        }
+
+        $this->reservationRepository->save($numero, $date, $heurePrevu, $nombreConvive, $allergie);
+
+        return new JsonResponse(['status' => 'Reservation created!'], Response::HTTP_CREATED);
+    }
+
+    /**
+     * @Route("/reservations/{id}", name="get_one_reservation", methods={"GET"})
+     */
+    public function get($id): JsonResponse
+    {
+        $reservation = $this->reservationRepository->findOneBy(['id' => $id]);
+
+        $data = [
+            'id' => $reservation->getId(),
+            'numero' => $reservation->getNumero(),
+            'date'=>$reservation->getDate(),
+            'heurePrevu'=>$reservation->getHeurePrevu(),
+            'nombreConvive'=>$reservation->getNombreConvive(),
+            'allergie'=>$reservation->getAllergie(),
+        ];
+
+        return new JsonResponse($data, Response::HTTP_OK);
+    }
+
+    /**
+     * @Route("/reservations", name="get_all_reservations", methods={"GET"})
+     */
+    public function getAll(): JsonResponse
+    {
+        $reservations = $this->reservationRepository->findAll();
+        $data = [];
+
+        foreach ($reservations as $reservation) {
+            $data[] = [
+                'id' => $reservation->getId(),
+                'numero' => $reservation->getNumero(),
+                'date'=>$reservation->getDate(),
+                'heurePrevu'=>$reservation->getHeurePrevu(),
+                'nombreConvive'=>$reservation->getNombreConvive(),
+                'allergie'=>$reservation->getAllergie(),
+            ];
+        }
+
+        return new JsonResponse($data, Response::HTTP_OK);
+    }
+
+    /**
+     * @Route("/reservations/{id}", name="update_reservation", methods={"PUT"})
+     */
+    public function update($id, Request $request): JsonResponse
+    {
+        $reservation = $this->reservationRepository->findOneBy(['id' => $id]);
+        $data = json_decode($request->getContent(), true);
+
+        empty($data['numero']) ? true : $reservation->setNumero($data['numero']);
+        empty($data['date']) ? true : $reservation->setDate($data['date']);
+        empty($data['heurePrevu']) ? true : $reservation->setHeurePrevu($data['heurePrevu']);
+        empty($data['nombreConvive']) ? true : $reservation->setNombreConvive($data['nombreConvive']);
+        empty($data['allergie']) ? true : $reservation->setAllergie($data['allergie']);
+
+        $updatedReservation = $this->reservationRepository->update($reservation);
+
+        return new JsonResponse($updatedReservation, Response::HTTP_OK);
+    }
+
+    /**
+     * @Route("/reservations/{id}", name="delete_reservation", methods={"DELETE"})
+     */
+    public function delete($id): JsonResponse
+    {
+        $reservation = $this->reservationRepository->findOneBy(['id' => $id]);
+
+        $this->reservationRepository->remove($reservation);
+
+        return new JsonResponse(['status' => 'Reservation deleted'], Response::HTTP_NO_CONTENT);
     }
 }
